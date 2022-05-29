7c6470f139e561ea317f30bd603c9de7c0a6453e
MappiCode <morten.napiralla@gmx.de>
2022-05-30 00:45:08 +0200

    First Song

--- Journey of the Star Runner/Assets/UI/SettingsMenu.cs
+++ Journey of the Star Runner/Assets/UI/SettingsMenu.cs
@@ -2,10 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    private void Start()
+    {
+        Slider[] slider = FindObjectsOfType<Slider>();
+
+        float[] values = new float[slider.Length];
+
+        audioMixer.GetFloat("VolumeEffects", out values[0]);
+        audioMixer.GetFloat("VolumeMusic", out values[1]);
+        audioMixer.GetFloat("VolumeMaster", out values[2]);
+        
+        for(int i = 0; i < slider.Length; i++)
+        {
+            slider[i].value = values[i];
+        }
+    }
 
     public void SetVolumeMaster(float volume)
     {
